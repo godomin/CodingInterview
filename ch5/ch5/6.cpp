e7f354d16c92cfc10dfa8830e567c293dca37d3b
김영빈 <ykim000514@gmail.com>
2018-04-15 17:28:09 +0900

    #5.6 hint

---
+++ ch5/ch5/6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int getDiffBitCount(int a, int b) {
+    int diff = a^b;
+    int cnt = 0;
+    while(diff!=0) {
+        if(diff & 1)
+            cnt++;
+        diff = diff >> 1;
+    }
+    return cnt;
+}
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << getDiffBitCount(a, b) << endl;
+    return 0;
+}
